98c74eded6acc8d905947772b36ad236e7716a8a
thomdaro <31260388+thomdaro@users.noreply.github.com>
2023-03-02 15:46:03 -0500

    Prep for PyPI upload
    Rollback version number to conform to PyPI standards, add IU software license.

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -9,7 +9,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b227.1"
+        return "1.0b1"
 
     @property
     def __pretty_version__(self):
